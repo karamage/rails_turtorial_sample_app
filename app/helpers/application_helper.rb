ec0dcf187c5eb895dcf7f29c248e6c3a88bd75bd
karamage <karamage.umauma@gmail.com>
2023-12-13 08:40:54 +0900

    tweak.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  # ページごとの完全なタイトルを返す tweak tweak
+  # ページごとの完全なタイトルを返す tweak
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
