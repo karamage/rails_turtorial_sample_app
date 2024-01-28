9f1f78e56977937086568811b48e1bd304261b95
karamage <karamage.umauma@gmail.com>
2024-01-29 08:45:52 +0900

    list 12.15 over.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -17,4 +17,18 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  # 正しいユーザーかどうか確認する
+  def valid_user
+    unless (@user && @user.activated? &&
+      @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
+    end
+  end
 end
