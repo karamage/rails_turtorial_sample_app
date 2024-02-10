d4b1e633f6eb0d64d427affa044b935a3a0ad3d2
karamage <karamage.umauma@gmail.com>
2024-02-10 16:21:56 +0900

    list 13.75 over.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,7 +3,6 @@ class Micropost < ApplicationRecord
   has_one_attached :image do |attachable|
     attachable.variant :display, resize_to_limit: [500, 500]
   end
-  has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
