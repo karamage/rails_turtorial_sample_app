cd4c03cf490694688a64ca30c31a247687097dbf
karamage <karamage.umauma@gmail.com>
2024-02-10 16:20:13 +0900

    list 13.75 over.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_one_attached :image do |attachable|
+    attachable.variant :display, resize_to_limit: [500, 500]
+  end
   has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
