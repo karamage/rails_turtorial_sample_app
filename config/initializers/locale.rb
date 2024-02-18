84ac27dc9f49701ef53d5915d79927966f4cf7fe
karamage <karamage.umauma@gmail.com>
2024-02-18 15:52:51 +0900

    fixed i18n initialize and location locale file.

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,5 +1,6 @@
 # I18nライブラリに訳文の探索場所を指示する
-I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb, yml}')]
+# I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb, yml}')]
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
 # アプリケーションでの利用を許可するロケールのリストを渡す
 I18n.available_locales = [:en, :ja]
