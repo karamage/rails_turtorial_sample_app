0bac1dd6fe199c22ce32b4840142b22232ee87cd
karamage <karamage.umauma@gmail.com>
2024-02-22 15:28:11 +0900

    fixed rspec config.

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,8 @@ module SampleApp
     config.load_defaults 7.0
     config.active_storage.variant_processor = :mini_magick
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g| g.test_framework :rspec,
+                                              fixtures: false, view_specs: false, helper_specs: false, routing_specs: false
+    end
   end
 end
